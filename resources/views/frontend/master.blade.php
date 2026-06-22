39b2ee0a56a3cfe387112f8bca49243acd9f330a
taskin8651 <mdsayebalam10@gmail.com>
2026-06-22 11:00:18 +0530

    d

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -147,7 +147,7 @@
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="https://gdmm.tcspatna.in/" target="_blank" rel="noopener">Online-Admission</a></li>
               <li><a class="dropdown-item" href="https://gdmm.tcspatna.in/" target="_blank" rel="noopener">Online Fee</a></li>
-              <li><a class="dropdown-item" href="https://gdmm.tcspatna.in/" target="_blank" rel="noopener">CLC Apply</a></li>
+              <li><a class="dropdown-item" href="http://gdmmclc.tcspatna.in/" target="_blank" rel="noopener">CLC Apply</a></li>
             </ul>
           </li>
 
